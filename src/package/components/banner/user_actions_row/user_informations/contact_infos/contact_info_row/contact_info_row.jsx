cdc2190ac9d7084c52084a30a9b9ebff3340454a
Kevin Wang <dingfengyu@gmail.com>
2020-06-20 15:01:42 +0100

    Added mailto link to email section.

--- src/package/components/banner/user_actions_row/user_informations/contact_infos/contact_info_row/contact_info_row.jsx
+++ src/package/components/banner/user_actions_row/user_informations/contact_infos/contact_info_row/contact_info_row.jsx
@@ -12,6 +12,29 @@ const useStyles = createUseStyles(styles);
 export const ContactInfoRow = ({ icon: Icon, translation, value }) => {
     const classes = useStyles();
     const { formatMessage } = useIntl();
+
+    if (translation.defaultMessage === 'Email address') {
+        const mailto = 'mailto:'.concat(value);
+        return (
+            <div className={classes.container}>
+                <Tooltip title={formatMessage(translation)}>
+                    <button className={classes.button} type="button">
+                        <Icon className={classes.icon} />
+                    </button>
+                </Tooltip>
+                <a href={mailto}>
+                    <Typography
+                        customClasses={{
+                            container: classes.typography
+                        }}
+                        color="light"
+                    >
+                        {value}
+                    </Typography>
+                </a>
+            </div>
+        );
+    }
     return (
         <div className={classes.container}>
             <Tooltip title={formatMessage(translation)}>
